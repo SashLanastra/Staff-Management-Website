5b48d3fa3a5637ea6e0031874c8e2a09b7bb9f1e
Mpho Mankanda <mphomankanda95@gmail.com>
2023-09-15 21:57:59 +0200

    redundant changes

--- client/src/Layouts/HomeLayout.jsx
+++ client/src/Layouts/HomeLayout.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import logo from '../assets/logo.svg'
 import { Outlet } from 'react-router-dom'
 
 export const HomeLayout = () => {
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
+import logo from '../assets/logo.svg'
 
 export const Home = () => {
 
